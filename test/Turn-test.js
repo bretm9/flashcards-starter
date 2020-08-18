5d831fe1eaf70221955f2b287217127bcbb6da35
Bret Merritt <merrittbret9@gmail.com>
2020-08-17 18:27:52 -0600

    add returnCard method

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -24,10 +24,17 @@ describe('Turn', function() {
   it('should store a card object', function() {
     const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     const turn = new Turn('object', card);
+    expect(turn.cardInPlay).to.equal(card);
   });
 
   it('should have a method returnGuess that returns the guess', function() {
     const turn = new Turn('object');
     expect(turn.returnGuess()).to.equal('object');
+  });
+
+  it('should have a method returnCard that returns the Card', function() {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const turn = new Turn('object', card);
+    expect(turn.returnCard()).to.equal(card);
   });
 });
