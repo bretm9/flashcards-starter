d2c01103a524da92a31ebf088cfa3b41de1acd03
Bret Merritt <merrittbret9@gmail.com>
2020-08-18 13:35:12 -0600

    Add Round class

---
+++ test/Round-test.js
@@ -0,0 +1,17 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Round = require('../src/Round');
+const { it } = require('mocha');
+
+describe('Round', function() {
+  it('should be a function', function() {
+    const round = new Round();
+    expect(Round).to.be.a('function');
+  });
+
+  it('should be an instance of Round', function() {
+    const round = new Round();
+    expect(round).to.be.an.instanceof(Round);
+  }); 
+})
