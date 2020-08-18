e80211197f67b34e4e80c21f429faaf26261d622
Bret Merritt <merrittbret9@gmail.com>
2020-08-18 11:56:23 -0600

    Add countCards method

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -24,4 +24,13 @@ describe('Deck', function() {
     const deck = new Deck(cards);
     expect(deck.cards).to.equal(cards);
   });
+
+  it('should have a method countCards that counts how many cards are in the deck', function() {
+    const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const card2 = new Card(2, "What's a food?", ['object', 'array', 'pizza'], 'pizza');
+    const card3 = new Card(3, "What's my favorite food?", ['pizza', 'sushi', 'oatmeal'], 'sushi');
+    const cards = [card1, card2, card3];
+    const deck = new Deck(cards);
+    expect(deck.countCards()).to.equal(3);
+  });
 });
