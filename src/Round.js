862db0a39e1c57a8855c3d59ff939b261ed02a33
Bret Merritt <merrittbret9@gmail.com>
2020-08-18 21:23:52 -0600

    Add takeTurn Method

--- src/Round.js
+++ src/Round.js
@@ -1,10 +1,25 @@
+const Turn = require("./Turn");
+
 class Round {
   constructor(deck) {
     this.deck = deck;
+    this.turns = 0;
+    this.incorrectGuesses = [];
   }
 
   returnCurrentCard() {
     return this.deck.cards[0];
+  }
+
+  takeTurn(guess) {
+    this.turns ++;
+    let newTurn = new Turn(guess, this.returnCurrentCard());
+    newTurn.id = this.turns;
+    this.deck.cards.shift();
+    if (!newTurn.evaluateGuess()) {
+      this.incorrectGuesses.push(newTurn.id);
+    }
+    return newTurn.giveFeedback();
   }
 }
 
--- test/Round-test.js
+++ test/Round-test.js
@@ -36,4 +36,78 @@ describe('Round', function() {
     const round = new Round(deck);
     expect(round.returnCurrentCard()).to.equal(deck.cards[0]);
   });
+
+  it('should have a method takeTurn', function() {
+    const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const card2 = new Card(2, "What's a food?", ['object', 'array', 'pizza'], 'pizza');
+    const card3 = new Card(3, "What's my favorite food?", ['pizza', 'sushi', 'oatmeal'], 'sushi');
+    const cards = [card1, card2, card3];
+    const deck = new Deck(cards);
+    const round = new Round(deck);
+    expect(round.takeTurn).to.be.a('function');
+  });
+
+  it('takeTurn should update turns count', function() {
+    const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const card2 = new Card(2, "What's a food?", ['object', 'array', 'pizza'], 'pizza');
+    const card3 = new Card(3, "What's my favorite food?", ['pizza', 'sushi', 'oatmeal'], 'sushi');
+    const cards = [card1, card2, card3];
+    const deck = new Deck(cards);
+    const round = new Round(deck);
+    round.takeTurn('pizza');
+    expect(round.turns).to.equal(1);
+  });
+  
+  it('takeTurn should evaluate an incorrect guess', function() {
+    const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const card2 = new Card(2, "What's a food?", ['object', 'array', 'pizza'], 'pizza');
+    const card3 = new Card(3, "What's my favorite food?", ['pizza', 'sushi', 'oatmeal'], 'sushi');
+    const cards = [card1, card2, card3];
+    const deck = new Deck(cards);
+    const round = new Round(deck);
+    round.takeTurn('pizza');
+    expect(round.incorrectGuesses.length).to.equal(1);
+  });
+
+  it('takeTurn should evaluate a correct guess', function() {
+    const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const card2 = new Card(2, "What's a food?", ['object', 'array', 'pizza'], 'pizza');
+    const card3 = new Card(3, "What's my favorite food?", ['pizza', 'sushi', 'oatmeal'], 'sushi');
+    const cards = [card1, card2, card3];
+    const deck = new Deck(cards);
+    const round = new Round(deck);
+    round.takeTurn('object');
+    expect(round.incorrectGuesses.length).to.equal(0);
+  });
+  
+  it('takeTurn should store ids of incorrect guesses', function() {
+    const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const card2 = new Card(2, "What's a food?", ['object', 'array', 'pizza'], 'pizza');
+    const card3 = new Card(3, "What's my favorite food?", ['pizza', 'sushi', 'oatmeal'], 'sushi');
+    const cards = [card1, card2, card3];
+    const deck = new Deck(cards);
+    const round = new Round(deck);
+    round.takeTurn('pizza');
+    expect(round.incorrectGuesses[0]).to.equal(round.turns);
+  });
+  
+  it('takeTurn should give appropriate feedback when guess is correct', function() {
+    const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const card2 = new Card(2, "What's a food?", ['object', 'array', 'pizza'], 'pizza');
+    const card3 = new Card(3, "What's my favorite food?", ['pizza', 'sushi', 'oatmeal'], 'sushi');
+    const cards = [card1, card2, card3];
+    const deck = new Deck(cards);
+    const round = new Round(deck);
+    expect(round.takeTurn('object')).to.equal('correct!');
+  });
+
+  it('takeTurn should give appropriate feedback when guess is incorrect', function() {
+    const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const card2 = new Card(2, "What's a food?", ['object', 'array', 'pizza'], 'pizza');
+    const card3 = new Card(3, "What's my favorite food?", ['pizza', 'sushi', 'oatmeal'], 'sushi');
+    const cards = [card1, card2, card3];
+    const deck = new Deck(cards);
+    const round = new Round(deck);
+    expect(round.takeTurn('pizza')).to.equal('incorrect!');
+  });
 });
