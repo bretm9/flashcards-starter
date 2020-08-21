aad34cde5e97687e710666fd4f33cd51d6c80736
Bret Merritt <merrittbret9@gmail.com>
2020-08-20 18:27:34 -0600

    Refactor Game tests to use beforeEach

--- test/Game-test.js
+++ test/Game-test.js
@@ -9,51 +9,50 @@ const data = require('../src/data');
 const prototypeQuestions = data.prototypeData;
 
 describe('Game', function() {
+  let game;
+  let deck;
+
+  beforeEach(() => {
+    game = new Game();
+    deck = new Deck();
+  });
+
   it('should be a function', function() {
     expect(Game).to.be.a('function');
   });
 
   it('should be an instance of game', function() {
-    const game = new Game();
     expect(game).to.be.an.instanceof(Game);
   });
 
   it('should have a method buildDeck', function() {
-    const game = new Game();
     expect(game.buildDeck).to.be.a('function');
   });
 
   it('buildDeck should build a deck', function () {
-    const game = new Game();
     expect(game.buildDeck(prototypeQuestions)).to.be.an.instanceof(Deck);
   });
 
   it('buildDeck deck should be full of cards', function () {
-    const game = new Game();
-    const deck = game.buildDeck(prototypeQuestions);
+    let deck = game.buildDeck(prototypeQuestions);
     expect(deck.cards[0]).to.be.an.instanceof(Card);
   });
 
   it('should have a method beginRound', function() {
-    const game = new Game();
     expect(game.beginRound).to.be.a('function');
   });
 
   it('beginRound should create a new Round and store it in currentRound', function () {
-    const game = new Game();
     game.beginRound()
     expect(game.currentRound).to.be.an.instanceof(Round);
   });
 
   it('Round created with beginRound should include a deck', function () {
-    const game = new Game();
-    const deck = new Deck();
     game.beginRound(deck);
     expect(game.currentRound.deck).to.equal(deck);
   });
 
   it('should have a method start', function() {
-    const game = new Game();
     expect(game.start).to.be.a('function');
   });
 });
